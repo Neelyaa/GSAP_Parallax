d965451713b6275fd846436fa9bb0b2a185af793
Nelya Nouader <nelya-13@hotmail.fr>
2024-03-14 16:56:18 +0100

    update

--- js/script.js
+++ js/script.js
@@ -2,8 +2,22 @@ document.addEventListener('DOMContentLoaded', function () {
     'use-strict';
 
     Splitting();
-    
+
     gsap.registerPlugin(ScrollTrigger);
+
+    const lenis = new Lenis()
+
+    lenis.on('scroll', (e) => {
+        console.log(e)
+    })
+
+    lenis.on('scroll', ScrollTrigger.update)
+
+    gsap.ticker.add((time) => {
+        lenis.raf(time * 1000)
+    })
+
+    gsap.ticker.lagSmoothing(0)
 
     //Header
     const gTl = gsap.timeline();
